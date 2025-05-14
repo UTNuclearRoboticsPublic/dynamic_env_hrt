1a483c3f79efefaae138715fb82e494dea84be20
JD <john.a.duncan@utexas.edu>
2025-05-13 22:15:11 -0500

    Add mark_event button for researcher

--- scripts/record_data.sh
+++ scripts/record_data.sh
@@ -7,4 +7,4 @@ if [ -z "$1" ]; then
 fi
 
 # Create the file using the touch command
-ros2 bag record -s mcap -o ../bags/"$1" /cmd_vel_operator /joy_supervisor /panther/odometry/filtered /panther/dr_spaam_detections /joy_teammate /trial_start /trial_end /joy_researcher /manual /dr_spaam_rviz /joy_operator /estop /cmd_vel_researcher
+ros2 bag record -s mcap -o ../bags/"$1" /cmd_vel_operator /joy_supervisor /panther/odometry/filtered /panther/dr_spaam_detections /joy_teammate /trial_start /mark_event /trial_end /joy_researcher /disable_auto /dr_spaam_rviz /joy_operator /estop /cmd_vel_researcher
